49ae12a799603aae7eb04e372c6bf4c9fe04bc60
psjoao <paulo.sabino.joao@gmail.com>
2026-01-04 23:41:38 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo com Git!");
+            Console.WriteLine("Meu primeiro projeto versionado.");
+
+            Console.ReadLine();
+        }
+    }
+}
